507e404c80df11e270e3e322d91b9b569097b8e4
いっちー <160448478+Ichihara-Soma@users.noreply.github.com>
2024-06-25 15:42:24 +0900

    add: slideHandle navigate 完成
    Co-authored-by: ngtnok <ngtnok@users.noreply.github.com>

--- frontend/src/components/Presentation.tsx
+++ frontend/src/components/Presentation.tsx
@@ -1,49 +1,70 @@
 import {FC, MutableRefObject, useEffect, useRef} from "react";
+import { useNavigate } from "react-router-dom";
 import {Button, Center} from '@mantine/core';
 import "./Presentation.scss"
 
+interface SlideResult {
+    countPercentage: number,
+    elapsedTime: number
+}
+
 const Presentation: FC = () => {
-    let countValiable: MutableRefObject<number> = useRef(0);
+    const navigate = useNavigate();
+
+    let countVariable: MutableRefObject<number> = useRef(0);
     let countAll: MutableRefObject<number> = useRef(0);
-    let slideStartTime:MutableRefObject<number> = useRef(0)
-    let countPercentage : MutableRefObject<number> = useRef(0)
-    let elapsedTime : MutableRefObject<number> = useRef(0)
+    let slideStartTime: MutableRefObject<number> = useRef(0)
+    let countPercentage: MutableRefObject<number> = useRef(0)
+    let elapsedTime: MutableRefObject<number> = useRef(0)
+    const arrSlideResult: SlideResult[] = [];
 
     useEffect(() => {
         const webgazer = window.webgazer;
         webgazer
             .setGazeListener((data, clock) => {
-                data.y < 300 && ++countValiable.current;
+                data.y < 300 && ++countVariable.current;
                 ++countAll.current;
-                console.log("countValiable : ", countValiable)
+                console.log("countVariable : ", countVariable)
                 console.log("countAll      : ", countAll)
             })
             .begin();
     }, [])
 
-    const startHundle = () => {
-        countValiable.current = 0;
+    const startHandle = () => {
+        countVariable.current = 0;
         countAll.current = 0;
         slideStartTime.current = Number(new Date())
     }
 
-    const stopHundle = () => {
-        countPercentage.current = Math.floor((countValiable.current * 100)/countAll.current);
+    const slideHandle = () => {
+        countPercentage.current = Math.floor((countVariable.current * 100) / countAll.current);
         elapsedTime.current = Number(new Date()) - slideStartTime.current;
-        console.log("countPercentage :" , countPercentage.current)
-        console.log("elapsedTime :" , elapsedTime.current)
-        const webgazer = window.webgazer;
-        webgazer.end();
+        arrSlideResult.push({countPercentage: countPercentage.current, elapsedTime: elapsedTime.current})
+        // console.log("countPercentage :", countPercentage.current)
+        // console.log("elapsedTime :", elapsedTime.current)
+        startHandle();
     }
 
+    const stopHandle = () => {
+        slideHandle();
+        // countPercentage.current = Math.floor((countVariable.current * 100)/countAll.current);
+        // elapsedTime.current = Number(new Date()) - slideStartTime.current;
+        // console.log("countPercentage :" , countPercentage.current)
+        // console.log("elapsedTime :" , elapsedTime.current)
+        const webgazer = window.webgazer;
+        webgazer.end();
+        console.log(arrSlideResult);
+        navigate("/result", {state: {result: arrSlideResult}})
+    }
 
 
     return (
         <>
             <Center>
                 <h1>Presentationページです</h1>
-                <Button onClick={startHundle}>スタート</Button>
-                <Button onClick={stopHundle}>ストップ</Button>
+                <Button onClick={startHandle}>スタート</Button>
+                <button onClick={slideHandle}>スライドめくる</button>
+                <Button onClick={stopHandle}>ストップ</Button>
             </Center>
         </>
     )
