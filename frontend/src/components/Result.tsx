629305fff03012ed5e47408dccece92955ece789
ngtnok <105690483+ngtnok@users.noreply.github.com>
2024-06-28 23:45:59 +0900

    add: eyeScore to BarChart

--- frontend/src/components/Result.tsx
+++ frontend/src/components/Result.tsx
@@ -1,7 +1,8 @@
 import type {FC} from "react";
 import {useLocation} from "react-router-dom";
 import {SlideResult} from "../global";
-import { RadarChart } from '@mantine/charts';
+import {RadarChart} from '@mantine/charts';
+import {BarChart} from '@mantine/charts';
 
 // const sampleResult = [{countPercentage: 80, speed: 50, positive: 50}, {
 //     countPercentage: 60,
@@ -12,19 +13,28 @@ import { RadarChart } from '@mantine/charts';
 const Result: FC = () => {
     const location = useLocation();
     console.log(location)
+    const data = useLocation().state.map((obj: SlideResult, idx: number)=> ({page: idx + 1, eyeScore:obj.countPercentage}))
 
     const countPages = location.state.length;
     console.log(countPages);
 
-    const totalEye = location.state.map(obj => {
+    const totalEye = location.state.map((obj: SlideResult)=> {
         return obj.countPercentage
-    }).reduce((acc, cur) => acc + cur, 0)
+    }).reduce((acc: number, cur:number) => acc + cur, 0)
     console.log(totalEye)
 
     const eyeScore = Math.floor(totalEye / countPages);
     console.log(eyeScore);
 
-    const scoreData = [{product: '目線', score: eyeScore, threshold: 80}, {product: '速度', score: 30,threshold: 80}, {product: 'ハキハキ', score: 40, threshold: 80}, {product: '時間', score: 90, threshold: 80}, {product: '繋ぎ言葉', score: 100, threshold: 80}]
+    const scoreData = [{product: '目線', score: eyeScore, threshold: 80}, {
+        product: '速度',
+        score: 30,
+        threshold: 80
+    }, {product: 'ハキハキ', score: 40, threshold: 80}, {
+        product: '時間',
+        score: 90,
+        threshold: 80
+    }, {product: '繋ぎ言葉', score: 100, threshold: 80}]
 
 
     return (
@@ -33,12 +43,33 @@ const Result: FC = () => {
             <RadarChart h={300}
                         data={scoreData}
                         dataKey="product"
-                        series={[{ name: 'score', color: 'blue.4', opacity: 0.2 }, { name: 'threshold', color: 'red.4', opacity: 0.2 }]}
+                        series={[{name: 'score', color: 'blue.4', opacity: 0.2}, {
+                            name: 'threshold',
+                            color: 'red.4',
+                            opacity: 0.2
+                        }]}
                         withPolarGrid
                         withPolarAngleAxis
             />
-            {location.state.map((result: SlideResult, index: number) => <p
-                key={index}>{result.countPercentage} {result.elapsedTime}</p>)}
+            <BarChart
+                h={250}
+                w={700}
+                data={data}
+                dataKey="page"
+                type="stacked"
+                withLegend
+                referenceLines={[
+                    {
+                        y: 80,
+                        color: 'red.5',
+                        labelPosition: 'insideTopRight',
+                    },
+                ]}
+                legendProps={{verticalAlign: 'bottom'}}
+                series={[
+                    {name: 'eyeScore', label: '目線がカメラを向いているか', color: 'blue.4'},
+                ]}
+            />
         </>
     )
 }
