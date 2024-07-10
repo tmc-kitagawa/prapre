12c55ff7ba89e29d704dfc18c0df244259ff56f0
tmc-kitagawa <masahiro_kitagawa@mail.toyota.co.jp>
2024-07-10 18:46:45 +0900

    refactor path from calibration to presentation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import {Route, Routes} from "react-router-dom";
 import Home from "./components/Home";
-import Calibration from "./components/Calibration";
 import Result from "./components/Result";
 import {MantineProvider} from "@mantine/core";
 import "@mantine/core/styles.css";
 import '@mantine/dropzone/styles.css';
 import {useState} from "react";
+import Presentation from "./components/Presentation.tsx";
 
 const App = () => {
     const [userId, setUserId] = useState<null | number>(null)
@@ -18,7 +18,7 @@ const App = () => {
         <MantineProvider>
             <Routes>
                 <Route path="/" element={<Home setUserId={setUserId} slide={slide} setSlide={setSlide} setPresentationTime={setPresentationTime}/>}/>
-                <Route path="calibration" element={<Calibration slide={slide} presentationTime={presentationTime} setFillers={setFillers} setVolumes={setVolumes}/>}/>
+                <Route path="presentation" element={<Presentation slide={slide} presentationTime={presentationTime} setFillers={setFillers} setVolumes={setVolumes}/>}/>
                 <Route path="result" element={<Result userId={userId} fillers={fillers} volumes={volumes} presentationTime={presentationTime} slide={slide}/>}/>
             </Routes>
         </MantineProvider>
